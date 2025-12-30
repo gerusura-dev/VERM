a87298341c8bd3e73c2f65c55389a342ad5badac
gerusura-dev <gerusuraimu@mail.gerusuraimu.com>
2025-12-30 22:30:04 +0900

    builder引数チェック追加

--- utils/builder.py
+++ utils/builder.py
@@ -26,16 +26,7 @@ class Payload:
         end_date_time: datetime,
         mode: Mode
     ) -> None:
-        today = datetime.now()
-
-        if start_date_time < today:
-            raise ValueError
-
-        if end_date_time < today:
-            raise ValueError
-
-        if end_date_time < start_date_time:
-            raise ValueError
+        self.__valid(event_name, platform, start_date_time, end_date_time, mode)
 
         self.event_name = event_name
         self.platform = platform
@@ -51,6 +42,40 @@ class Payload:
             Params.EndDate.build(self.end_date_time),
             Params.Mode.build(self.mode)
         )
+
+    @staticmethod
+    def __valid(
+        event_name: str,
+        platform: Platform,
+        start_date_time: datetime,
+        end_date_time: datetime,
+        mode: Mode
+    ):
+        if not isinstance(event_name, str):
+            raise TypeError
+
+        if not isinstance(platform, Platform):
+            raise TypeError
+
+        if not isinstance(start_date_time, datetime):
+            raise TypeError
+
+        if not isinstance(end_date_time, datetime):
+            raise TypeError
+
+        if not isinstance(mode, Mode):
+            raise TypeError
+
+        today = datetime.now()
+
+        if start_date_time < today:
+            raise ValueError
+
+        if end_date_time < today:
+            raise ValueError
+
+        if end_date_time < start_date_time:
+            raise ValueError
 
     @property
     def url(self) -> str:
